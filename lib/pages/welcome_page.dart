e476bc575080f00a35fa28a916c65888ecc17f1f
Ayan <ayan123chowdhury@gmail.com>
2022-09-27 00:03:04 +0530

    Pushed

---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app/Misc/colors.dart';
+// import 'package:flutter/rendering.dart';
+import 'package:travel_app/widgets/app_large_text.dart';
+import 'package:travel_app/widgets/app_text.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  List images = [
+    "welcome-one.png",
+    "welcome-two.png",
+    "welcome-three.png",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: PageView.builder(
+      scrollDirection: Axis.vertical,
+      itemCount: images.length,
+      itemBuilder: (_, index) {
+        return Container(
+          width: double.maxFinite,
+          height: double.maxFinite,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                "images/" + images[index],
+              ),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Container(
+            margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
+            child: Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    AppLargeText(text: "Trips"),
+                    // AppLargeText(text: "Discover", color: Colors.red),
+                    AppText(
+                      text: "Mountain",
+                      size: 30,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      width: 250,
+                      child: AppText(
+                        text:
+                            "Mountain hikes give you a incredible sense of freedom along with endurance test",
+                        color: AppColors.textColor2,
+                        size: 14,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    ));
+  }
+}
